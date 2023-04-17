ba5b090d65908ffcf956ef8e8f609aaee0fcdbdd
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-04-18 00:24:58 +0530

    REVERTING previous commit codes

--- src/style-guide/page-components/projectCardSection/index.jsx
+++ src/style-guide/page-components/projectCardSection/index.jsx
@@ -1,43 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./style.module.scss";
 import ProjectCard from "@/style-guide/components/ProjectCard";
-// import data from "../../../../projects.json";
+import data from "../../../../projects.json";
 
 const ProjectCardSection = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("/api/projects")
-      .then((req) => req.json())
-      .then((res) => {
-        setData(res.data);
-      });
-  }, []);
-  if (data.length > 0) {
-    return (
-      <div className={styles.ProjectCardSection}>
-        <h2>My Projects</h2>
-        <div className={styles.cards}>
-          {/* map for cards */}
-          {data.map((ele) => {
-            return (
-              <ProjectCard
-                title={ele.title}
-                talk={ele.talk}
-                techStack={ele.techStack}
-                id={ele.id}
-                link={ele.link}
-              />
-            );
-          })}
-        </div>
-        <a href="https://github.com/dev-AshishRanjan" className="btn">
-          For More
-        </a>
+  return (
+    <div className={styles.ProjectCardSection}>
+      <h2>My Projects</h2>
+      <div className={styles.cards}>
+        {/* map for cards */}
+        {data.map((ele) => {
+          return (
+            <ProjectCard
+              title={ele.title}
+              talk={ele.talk}
+              techStack={ele.techStack}
+              id={ele.id}
+              link={ele.link}
+            />
+          );
+        })}
       </div>
-    );
-  } else {
-    return null;
-  }
+      <a href="https://github.com/dev-AshishRanjan" className="btn">
+        For More
+      </a>
+    </div>
+  );
 };
 
 export default ProjectCardSection;
