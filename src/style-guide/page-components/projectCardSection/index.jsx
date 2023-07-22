2555f03de358c0414568c2dec1929c62f2de29a1
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-07-22 20:06:21 +0530

    ADD : Drop-Shadow

--- src/style-guide/page-components/projectCardSection/index.jsx
+++ src/style-guide/page-components/projectCardSection/index.jsx
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import styles from "./style.module.scss";
 import ProjectCard from "@/style-guide/components/ProjectCard";
-import data from "../../../../projects.json";
+// import data from "../../../../projects.json";
 import Spinner from "@/style-guide/components/spinner";
 
 const ProjectCardSection = () => {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   try {
-  //     fetch("/api/projects")
-  //       .then((req) => req.json())
-  //       .then((res) => {
-  //         setData(res.data);
-  //         setLoading(false);
-  //       });
-  //   } catch (e) {
-  //     alert("Error: " + e.message);
-  //   }
-  // }, []);
+  useEffect(() => {
+    try {
+      fetch("/api/projects")
+        .then((req) => req.json())
+        .then((res) => {
+          setData(res.data);
+          setLoading(false);
+        });
+    } catch (e) {
+      alert("Error: " + e.message);
+    }
+  }, []);
   return (
     <>
       {loading ? (
